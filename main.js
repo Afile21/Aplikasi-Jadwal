90914406382e8ed64ca63eb2e8a1187d2da23208
Alif Darmawan <alifikrimah21@gmail.com>
2026-03-09 19:54:54 +0700

    versi pertama Aplikasi Jadwal

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 700,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
+    // Menyembunyikan menu bar bawaan agar terlihat lebih modern
+    win.setMenuBarVisibility(false);
+
+    win.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
