6ff0187368581971fdbd4d9087c1b03eb0af5f1e
Alif Darmawan <alifikrimah21@gmail.com>
2026-03-10 10:42:42 +0700

    tahap level 1

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+require("electron-reload")(__dirname);
+const Database = require("better-sqlite3"); // Memanggil modul database
+
+// 1. Inisialisasi Database (Akan otomatis membuat file database.db jika belum ada)
+const dbPath = path.join(__dirname, "database.db");
+const db = new Database(dbPath);
+
+// 2. Membuat Tabel (Hanya dibuat jika tabel belum ada)
+db.exec(`
+    CREATE TABLE IF NOT EXISTS kategori (
+        id_kategori INTEGER PRIMARY KEY AUTOINCREMENT,
+        nama_kategori TEXT NOT NULL,
+        kode_warna TEXT NOT NULL
+    );
+    
+    CREATE TABLE IF NOT EXISTS jadwal (
+        id_jadwal INTEGER PRIMARY KEY AUTOINCREMENT,
+        id_kategori INTEGER,
+        judul_aktivitas TEXT NOT NULL,
+        tanggal TEXT NOT NULL,
+        waktu_mulai TEXT NOT NULL,
+        waktu_selesai TEXT NOT NULL,
+        status TEXT DEFAULT 'Belum Mulai',
+        is_berulang INTEGER DEFAULT 0,
+        FOREIGN KEY (id_kategori) REFERENCES kategori (id_kategori)
+    );
+`);
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -15,6 +42,8 @@ function createWindow() {
     win.setMenuBarVisibility(false);
 
     win.loadFile("index.html");
+
+    //win.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
@@ -28,6 +57,8 @@ app.whenReady().then(() => {
 });
 
 app.on("window-all-closed", () => {
+    // Menutup koneksi database saat aplikasi ditutup
+    db.close(); 
     if (process.platform !== "darwin") {
         app.quit();
     }
