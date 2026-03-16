e815759470d91eabef71a40674c700512eacbcce
Alif Darmawan <alifikrimah21@gmail.com>
2026-03-16 14:50:22 +0700

    penambahan schema db

--- schema.sql
+++ schema.sql
@@ -6,7 +6,8 @@ CREATE TABLE IF NOT EXISTS "jadwal" (
 	"tanggal"	TEXT NOT NULL,
 	"waktu_mulai"	TEXT NOT NULL,
 	"waktu_selesai"	TEXT NOT NULL,
-	"status"	TEXT DEFAULT 'Belum Mulai',
+	"prioritas"	TEXT DEFAULT 'Sedang',
+	"status"	TEXT DEFAULT 'To Do',
 	"is_berulang"	INTEGER DEFAULT 0,
 	PRIMARY KEY("id_jadwal" AUTOINCREMENT),
 	FOREIGN KEY("id_kategori") REFERENCES "kategori"("id_kategori")
@@ -16,5 +17,14 @@ CREATE TABLE IF NOT EXISTS "kategori" (
 	"nama_kategori"	TEXT NOT NULL,
 	"kode_warna"	TEXT NOT NULL,
 	PRIMARY KEY("id_kategori" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "rutinitas" (
+	"id_rutinitas"	INTEGER,
+	"id_kategori"	INTEGER,
+	"judul_aktivitas"	TEXT NOT NULL,
+	"waktu_mulai"	TEXT NOT NULL,
+	"waktu_selesai"	TEXT NOT NULL,
+	"prioritas"	TEXT DEFAULT 'Sedang',
+	PRIMARY KEY("id_rutinitas" AUTOINCREMENT)
 );
 COMMIT;
